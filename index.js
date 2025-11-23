e223238ad3a29f40d2272e77de2665f154f3e683
proboy315 <proboy0315@gmail.com>
2025-11-23 14:32:30 +0500

    Update version log from Raganork to ProBoy

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const {
 async function main() {
   ensureTempDir();
   logger.info(`Created temporary directory at ${TEMP_DIR}`);
-  console.log(`Raganork v${require("./package.json").version}`);
+  console.log(`ProBoy v${require("./package.json").version}`);
   console.log(`- Configured sessions: ${SESSION.join(", ")}`);
   logger.info(`Configured sessions: ${SESSION.join(", ")}`);
   if (SESSION.length === 0) {
